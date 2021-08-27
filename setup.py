4a924087d7e9fe3fa81d0b6df81e09437e4f0daa
MathieuLeocmach <mathieu.leocmach@gmail.com>
2021-08-27 14:21:38 +0200

    version 0.3.1 for pypi

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, 'readme.md'), encoding='utf-8') as f:
 
 setup(
     name='colloid_polymer_phase_diagram',
-    version='0.3.0',
+    version='0.3.1',
     description='A Python module to compute the phase behaviour of Colloid+Polymer mixture',
     long_description=long_description,
     long_description_content_type='text/markdown',
